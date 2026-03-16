94edb0897f4573bd2f1b357c3f42a7a95808cc46
RodriiPr <arodriguepr.04@hotmail.com>
2026-03-16 10:46:17 -0500

    feat: allow all origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,7 @@ app = FastAPI(title="Notes API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://app-notes-frontend.onrender.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
